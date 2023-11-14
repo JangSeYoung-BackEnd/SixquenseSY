041dd393099e71d6d1021b4073e2582dce7017e5
LeeKyuHong <LeeKyuHong@DESKTOP-1L9M056>
2023-11-14 13:24:36 +0900

    .

--- SixQuense_project/src/main/java/com/web/accompany/model/dto/AccompanyDTO.java
+++ SixQuense_project/src/main/java/com/web/accompany/model/dto/AccompanyDTO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.web.accompany.model.dto;
 
 import java.sql.Date;
@@ -19,25 +18,4 @@ public class AccompanyDTO {
 	private int coordinateNo;
 	private String openChattingLink;
 }
-=======
-package com.web.accompany.model.dto;
 
-import java.sql.Date;
-
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public class AccompanyDTO {
-	private int accompanyNo;
-	private String accompanyTitle;
-	private String accompanyContent;
-	private int accompanyCount;
-	private Date accompanyDate;
-	private int memberNo;
-	private int accompanyReadCount;
-	private int coordnateNo;
-	private String openChattingLink;
-}
->>>>>>> branch 'test' of https://github.com/ImmortalDeveloper/Sixquense.git

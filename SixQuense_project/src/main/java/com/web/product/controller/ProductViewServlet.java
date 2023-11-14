6aeac53dfd0ffaf54be1f4b369d32d031a06e124
archi <archi@DESKTOP-6FC56U4>
2023-11-14 14:16:47 +0900

    je_product 서블릿 추가, settings 제외

---
+++ SixQuense_project/src/main/java/com/web/product/controller/ProductViewServlet.java
@@ -0,0 +1,43 @@
+package com.web.product.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProductViewServlet
+ */
+@WebServlet("/product/productview.do")
+public class ProductViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductViewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		//나라/도시별 패키지 출력 화면 jsp로 이동
+		request.getRequestDispatcher("/views/product/productlistbycountry.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

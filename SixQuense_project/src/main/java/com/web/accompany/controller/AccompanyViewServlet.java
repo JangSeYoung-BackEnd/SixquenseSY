a07ba610d0bccf9193e19eb1f687519736944c3a
Julia <Julia@DESKTOP-3LUJ1TN>
2023-11-14 15:17:56 +0900

    wh_코드 수정

--- SixQuense_project/src/main/java/com/web/accompany/controller/AccompanyViewServlet.java
+++ SixQuense_project/src/main/java/com/web/accompany/controller/AccompanyViewServlet.java
@@ -35,37 +35,11 @@ public class AccompanyViewServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//글번호를 받아서 조회하도록 한다. 
 		int no = Integer.parseInt(request.getParameter("no"));
-		
-		//조회수 부분 -쿠키를 가져옴 
-//		Cookie[] cookies = request.getCookies();
-//		String readAccompany="";
-//		boolean readResult = false; //읽지 않았다.
-//		for(Cookie c : cookies) {
-//			String name = c.getName();
-//			if(name.equals("readAccompany")) {
-//				readAccompany = c.getValue();
-//				if(readAccompany.contains("|"+no+"|")) {
-//					//contain인 애를 찾았다 -> 그전에 읽어었다.
-//					readResult= true;
-//				}
-//				break; //true로 빠지고 반복문 종료
-//			}
-//		}
-//		
-//		if(!readResult) {
-//			//안읽었다면 ~ 번호 다시 저장 
-//			Cookie c = new Cookie("readBoard", readAccompany+"|"+no+"|");
-//			c.setMaxAge(60*60*24);
-//			response.addCookie(c);
-//		}
+	
 		
 		AccompanyDTO a = new AccompanyServiceWH().selectBoardByNo(no);
 		System.out.println(a);
-		//이 글번호에 대한 댓글들을 List에 저장함.
-		//List<AccompanyComment> comments= new AccompanyServiceWH().selectAccompanyComment(no);
-		
-		
-		//request.setAttribute("comments", comments);
+
 		request.setAttribute("board", a);
 		request.getRequestDispatcher("/views/accompany/accompanyview.jsp").forward(request, response);
 
--- SixQuense_project/src/main/java/com/web/accompany/service/AccompanyServiceWH.java
+++ SixQuense_project/src/main/java/com/web/accompany/service/AccompanyServiceWH.java
@@ -18,15 +18,6 @@ public class AccompanyServiceWH {
 		public AccompanyDTO selectBoardByNo(int no) {
 			Connection conn = getConnection();
 			AccompanyDTO accompanyView  = dao.selectAccompanyByNo(conn,no);
-			
-			if(accompanyView!=null) {
-				int result= dao.updateAccompanyReadCount(conn,no);
-				if(result>0) {
-					commit(conn);
-					accompanyView.setAccompanyReadCount(accompanyView.getAccompanyReadCount());
-				}
-				else rollback(conn);
-			}
 			close(conn);
 			return accompanyView;
 		}

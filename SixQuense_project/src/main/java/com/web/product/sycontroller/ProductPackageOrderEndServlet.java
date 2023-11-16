c846fdb730b3e80813e57992cf6497c79fa32b30
adam9 <adam9@DESKTOP-FJQL8H7>
2023-11-16 15:36:28 +0900

    Merge branch 'test' of https://github.com/ImmortalDeveloper/Sixquense.git into test

--- SixQuense_project/src/main/java/com/web/product/sycontroller/ProductPackageOrderEndServlet.java
+++ SixQuense_project/src/main/java/com/web/product/sycontroller/ProductPackageOrderEndServlet.java
@@ -12,8 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.web.product.SYservice.BookingService;
 import com.web.product.dto.BookinginfoDto;
+import com.web.product.syservice.BookingService;
 
 /**
  * Servlet implementation class ProductPackageOrderEndServlet
@@ -27,6 +27,7 @@ public class ProductPackageOrderEndServlet extends HttpServlet {
      */
     public ProductPackageOrderEndServlet() {
         super();
+        
         // TODO Auto-generated constructor stub
     }
 
